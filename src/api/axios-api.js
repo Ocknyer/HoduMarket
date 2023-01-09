335a89bd0e3d63f646048061b0c7871440f8ac82
Ocknyer <kdu4045@naver.com>
2023-01-09 14:53:54 +0900

    repo 연결

---
+++ src/api/axios-api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "https://openmarket.weniv.co.kr/";
+
+export const instance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application.json",
+    },
+});
+
+export const accessInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application.json",
+    },
+});
+
+accessInstance.interceptors.request.use((config) => {
+    if (!config.headers.Authorization) {
+        config.headers = {
+            ...config.headers,
+            Authorization: `JWT ${localStorage.getItem("token")}`,
+        };
+    }
+});
