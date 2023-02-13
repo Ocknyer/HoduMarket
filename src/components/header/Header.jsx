853ab6881e97fa978565252dc1e4e6d4e2f2405c
Ocknyer <kdu4045@naver.com>
2023-02-13 12:00:29 +0900

    feat: 장바구니, 마이페이지 링크 연결

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,41 +1,48 @@
-import { HeaderWrapper, IconWrapper } from "./styled";
-import logo from "../../assets/img/Logo-hodu.png";
-import cart from "../../assets/img/icon-shopping-cart.svg";
-import mypage from "../../assets/img/icon-user.svg";
-import sellerCenter from "../../assets/img/icon-shopping-bag.svg";
-import Button from "../common/Button/Button";
+import { HeaderWrapper, IconWrapper } from './styled';
+import logo from '../../assets/img/Logo-hodu.png';
+import cart from '../../assets/img/icon-shopping-cart.svg';
+import mypage from '../../assets/img/icon-user.svg';
+import sellerCenter from '../../assets/img/icon-shopping-bag.svg';
+import Button from '../common/Button/Button';
+import { Link } from 'react-router-dom';
 
 const Header = ({ userType }) => {
   console.log(userType);
 
   return (
     <HeaderWrapper>
-      <img src={logo} alt="호두마켓 로고" className="logo" />
+      <img src={logo} alt='호두마켓 로고' className='logo' />
       <form>
         <input
-          className="inp-search"
-          type="text"
-          placeholder="상품을 검색해보세요!"
+          className='inp-search'
+          type='text'
+          placeholder='상품을 검색해보세요!'
         />
-        <button className="btn-search" type="button"></button>
+        <button className='btn-search' type='button'></button>
       </form>
-      <div className="icon-group">
-        {userType === "BUYER" ? (
+      <div className='icon-group'>
+        {userType === 'BUYER' ? (
           <>
-            <IconWrapper>
-              <img src={cart} alt="" /> 장바구니
-            </IconWrapper>
-            <IconWrapper>
-              <img src={mypage} alt="" /> 마이페이지
-            </IconWrapper>
+            <Link to='/cart'>
+              <IconWrapper>
+                <img src={cart} alt='' /> 장바구니
+              </IconWrapper>
+            </Link>
+            <Link to='/mypage'>
+              <IconWrapper>
+                <img src={mypage} alt='' /> 마이페이지
+              </IconWrapper>
+            </Link>
           </>
         ) : (
           <>
-            <IconWrapper>
-              <img src={mypage} alt="" /> 마이페이지
-            </IconWrapper>
+            <Link to='/mypage'>
+              <IconWrapper>
+                <img src={mypage} alt='' /> 마이페이지
+              </IconWrapper>
+            </Link>
             <Button>
-              <img src={sellerCenter} alt="" />
+              <img src={sellerCenter} alt='' />
               판매자 센터
             </Button>
           </>
