8c80576533dc4e63f124f0f4a081f1c5bdc59a2a
Ocknyer <kdu4045@naver.com>
2023-02-18 20:44:23 +0900

    feat: 회원가입 섹션 마크업

--- src/components/User/UserSignUp/UserSignUp.jsx
+++ src/components/User/UserSignUp/UserSignUp.jsx
@@ -1,6 +1,7 @@
 import logo from '../../../assets/img/Logo-hodu.png';
+import Button from '../../common/Button/Button';
 import UserTab from '../UserTab/UserTab';
-import { SignUpWrapper } from './styled';
+import { SignupForm, SignUpWrapper } from './styled';
 
 const UserSignUp = ({ onClick }) => {
   return (
@@ -14,6 +15,46 @@ const UserSignUp = ({ onClick }) => {
           판매회원 로그인
         </button>
       </UserTab>
+      <SignupForm>
+        <label>
+          아이디
+          <input type='text' />
+          <Button size='sm' width='122px'>
+            중복확인
+          </Button>
+        </label>
+        <label>
+          비밀번호
+          <input type='text' />
+        </label>
+        <label>
+          비밀번호 재확인
+          <input type='text' />
+        </label>
+        <label>
+          이름
+          <input type='text' />
+        </label>
+        <label>
+          휴대폰번호
+          <input type='text' />
+          <input type='text' />
+          <input type='text' />
+        </label>
+        <label>
+          이메일
+          <input type='text' />@
+          <input type='text' />
+        </label>
+      </SignupForm>
+      <input type='checkbox' />
+      <p>
+        호두샵의 이용약관 및 개인정보처리방침에 대한 내용을 확인하였고
+        동의합니다.
+      </p>
+      <Button size='md' width='480px' bc='#c4c4c4'>
+        가입하기
+      </Button>
     </SignUpWrapper>
   );
 };
--- src/components/User/UserSignUp/styled.jsx
+++ src/components/User/UserSignUp/styled.jsx
@@ -12,3 +12,22 @@ export const SignUpWrapper = styled.div`
     margin-bottom: 70px;
   }
 `;
+
+export const SignupForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 35px 34px;
+  border: 1px solid var(--greyC4);
+  border-radius: 10px;
+  margin-bottom: 30px;
+  background-color: #fff;
+  z-index: 99;
+
+  > input {
+    height: 60px;
+    border: none;
+    border-bottom: 1px solid var(--greyC4);
+    margin-bottom: 6px;
+  }
+`;
