52b6f5f318c9416d90a53fed97ca133a603f6e2b
Ocknyer <kdu4045@naver.com>
2023-03-03 03:48:17 +0900

    feat: recoil-persist 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-import ProductDetail from './components/Products/ProductDetail/ProductDetail';
 import Cart from './pages/Cart/Cart';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
--- src/atoms.js
+++ src/atoms.js
@@ -1,6 +1,10 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 export const userTypeValue = atom({
   key: 'login_type',
   default: 'BUYER',
+  effects_UNSTABLE: [persistAtom],
 });
