5eccc2b980c56cb358418b1ccfae911c11e511e1
Ocknyer <kdu4045@naver.com>
2023-05-08 15:54:12 +0900

    feat: 대시보드 마크업

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,11 @@
-import { DashboardHeader } from './styleld';
-import Button from '../common/Button/Button';
+import {
+  DashboardHeader,
+  DashboardSidebar,
+  DashboardBody,
+  DashboardMain,
+} from './styleld';
 import DefaultWrapper from '../common/Wrapper/DefaultWrapper';
+import Plus from '../../assets/img/icon-plus.svg';
 
 const Dashboard = () => {
   return (
@@ -9,10 +14,34 @@ const Dashboard = () => {
         <h2>
           대시보드 <span>백엔드글로벌</span>
         </h2>
-        <Button size='md' width='168px'>
+        <button className='btn-add-product'>
+          <img
+            src={Plus}
+            alt='상품 추가 아이콘'
+            className='icon-plus'
+          />
           상품 업로드
-        </Button>
+        </button>
       </DashboardHeader>
+      <DashboardBody>
+        <nav>
+          <DashboardSidebar>
+            <li className='active'>판매중인 상품</li>
+            <li>주문/배송</li>
+            <li>문의/리뷰</li>
+            <li>통계</li>
+            <li>스토어 설정</li>
+          </DashboardSidebar>
+        </nav>
+        <DashboardMain>
+          <div className='data-header'>
+            <p className='product-info'>상품정보</p>
+            <p className='product-price'>판매가격</p>
+            <p className='edit-product'>수정</p>
+            <p className='delete-product'>삭제</p>
+          </div>
+        </DashboardMain>
+      </DashboardBody>
     </DefaultWrapper>
   );
 };
--- src/components/Dashboard/styleld.jsx
+++ src/components/Dashboard/styleld.jsx
@@ -16,4 +16,78 @@ export const DashboardHeader = styled.section`
       font-weight: 500;
     }
   }
+
+  .btn-add-product {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 168px;
+    font-size: 18px;
+    line-height: 54px;
+    color: white;
+    background-color: var(--main-color);
+    border-radius: 5px;
+
+    .icon-plus {
+      width: 30px;
+      margin-right: 9px;
+    }
+  }
+`;
+
+export const DashboardBody = styled.div`
+  display: flex;
+  gap: 30px;
+`;
+
+export const DashboardSidebar = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  height: 290px;
+  gap: 10px;
+
+  > li {
+    height: 50px;
+    border-radius: 5px;
+    line-height: 50px;
+    padding: 0 20px;
+    font-weight: 500;
+
+    &:hover {
+      background-color: #effff3;
+    }
+  }
+
+  .active {
+    background-color: var(--main-color);
+  }
+`;
+
+export const DashboardMain = styled.section`
+  width: 100%;
+  border: 1px solid var(--greyC4);
+  border-radius: 5px;
+
+  .data-header {
+    display: flex;
+    width: 100%;
+    padding: 18px 0;
+    text-align: center;
+    font-size: 18px;
+    border-bottom: 1px solid var(--greyC4);
+
+    .product-info {
+      width: 50%;
+    }
+
+    .product-price {
+      width: 30%;
+    }
+
+    .edit-product,
+    .delete-product {
+      width: 10%;
+    }
+  }
 `;
