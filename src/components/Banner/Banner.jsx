58c30df22e8d8e45f2dc5f6175c01448bdab7b06
SUNGWON <sungwon@SUNGWONui-noteubug.local>
2023-05-17 10:14:06 +0900

    feat: 배너 캐러셀 라이브러리 적용

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,42 @@
-import BannerWrapper from "./styled";
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@mui/material";
 
 const Banner = () => {
-  return <BannerWrapper></BannerWrapper>;
+  const imgList = [
+    {
+      id: 1,
+      url: "https://images.unsplash.com/photo-1587731556938-38755b4803a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2978&q=80",
+    },
+    {
+      id: 2,
+      url: "https://images.unsplash.com/photo-1620560691654-935b940dcacc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2970&q=80",
+    },
+    {
+      id: 3,
+      url: "https://images.unsplash.com/photo-1543622748-5ee7237e8565?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2970&q=80",
+    },
+    {
+      id: 4,
+      url: "https://images.unsplash.com/photo-1532673492-1b3cdb05d51b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+    },
+  ];
+
+  return (
+    <Carousel>
+      {imgList.map((item) => (
+        <Paper
+          component="img"
+          key={item.id}
+          src={item.url}
+          sx={{
+            width: "100%",
+            height: "500px",
+            objectFit: "cover",
+          }}
+        ></Paper>
+      ))}
+    </Carousel>
+  );
 };
 
 export default Banner;
--- src/components/Banner/styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const BannerWrapper = styled.section`
-  width: 100%;
-  height: 500px;
-  background-color: var(--greyF2);
-`;
-
-export default BannerWrapper;
