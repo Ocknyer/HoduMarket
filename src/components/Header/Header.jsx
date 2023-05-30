8a173089ff9356ea51ce9620e5cb0b4325cb08bc
Ocknyer <kdu4045@naver.com>
2023-05-30 16:16:13 +0900

    fix: 검색 창 굵기 조정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,23 @@
-import logo from "../../assets/img/Logo-hodu.png";
-import cart from "../../assets/img/icon-shopping-cart.svg";
-import cartGreen from "../../assets/img/icon-shopping-cart-2.svg";
-import mypage from "../../assets/img/icon-user.svg";
-import sellerCenter from "../../assets/img/icon-shopping-bag.svg";
-import Button from "../common/Button/Button";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import search from "../../api/search/search";
-import { useState } from "react";
-import { useResetRecoilState, useSetRecoilState } from "recoil";
-import { onSearch, searchProducts } from "../../atoms";
-import { Box, IconButton, InputBase, Paper, Typography } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import logo from '../../assets/img/Logo-hodu.png';
+import cart from '../../assets/img/icon-shopping-cart.svg';
+import cartGreen from '../../assets/img/icon-shopping-cart-2.svg';
+import mypage from '../../assets/img/icon-user.svg';
+import sellerCenter from '../../assets/img/icon-shopping-bag.svg';
+import Button from '../common/Button/Button';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import search from '../../api/search/search';
+import { useState } from 'react';
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
+import { onSearch, searchProducts } from '../../atoms';
+import { Box, IconButton, InputBase, Paper, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = ({ userType }) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   const navigate = useNavigate();
   const location = useLocation();
 
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
   const setSearchData = useSetRecoilState(searchProducts);
   const resetSearchData = useResetRecoilState(searchProducts);
   const setSearchCount = useSetRecoilState(onSearch);
@@ -27,7 +27,7 @@ const Header = ({ userType }) => {
   };
 
   const handleSellerCenter = () => {
-    navigate("/sellercenter/dashboard/salesproduct");
+    navigate('/sellercenter/dashboard/salesproduct');
   };
 
   const searchProduct = () => {
@@ -42,13 +42,13 @@ const Header = ({ userType }) => {
   };
 
   const onSubmitSearch = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       e.preventDefault();
       search(inputValue)
         .then((data) => {
           setSearchCount(true);
           setSearchData(data.results);
-          navigate("/");
+          navigate('/');
         })
         .catch((error) => {
           console.log(error);
@@ -58,58 +58,53 @@ const Header = ({ userType }) => {
 
   const onClickHome = () => {
     resetSearchData();
-    setInputValue("");
+    setInputValue('');
     setSearchCount(false);
   };
 
   const iconWrapper = {
-    display: "flex",
-    flexDirection: "column",
-    gap: "4px",
-    alignItems: "center",
-    fontSize: "12px",
-    color: "text.grey",
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '4px',
+    alignItems: 'center',
+    fontSize: '12px',
+    color: 'text.grey',
   };
 
   const innerWrapper = {
-    width: "1280px",
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    margin: "0 auto",
+    width: '1280px',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    margin: '0 auto',
   };
 
   return (
     <Box
-      component="header"
+      component='header'
       sx={{
-        width: "100%",
-        height: "90px",
-        backgroundColor: "#fff",
-        boxShadow: "0 3px 5px rgba(0, 0, 0, 0.1)",
+        width: '100%',
+        height: '90px',
+        backgroundColor: '#fff',
+        boxShadow: '0 3px 5px rgba(0, 0, 0, 0.1)',
       }}
     >
-      {!location.pathname.includes("/sellercenter") ? (
+      {!location.pathname.includes('/sellercenter') ? (
         <Box sx={innerWrapper}>
-          <Link to="/" onClick={() => onClickHome()}>
-            <Box
-              component="img"
-              src={logo}
-              alt="호두마켓 로고"
-              width={"124px"}
-            />
+          <Link to='/' onClick={() => onClickHome()}>
+            <Box component='img' src={logo} alt='호두마켓 로고' width={'124px'} />
           </Link>
           <Paper
-            component="form"
+            component='form'
             elevation={0}
             sx={{
-              p: "2px 10px",
-              display: "flex",
-              width: "400px",
-              border: "2px solid",
-              borderColor: "border.main",
-              borderRadius: "50px",
-              ml: "30px",
+              p: '2px 10px',
+              display: 'flex',
+              width: '400px',
+              border: '3px solid',
+              borderColor: 'border.main',
+              borderRadius: '50px',
+              ml: '30px',
             }}
           >
             <InputBase
@@ -117,66 +112,63 @@ const Header = ({ userType }) => {
                 ml: 1,
                 flex: 1,
               }}
-              placeholder="상품을 검색해 보세요!"
+              placeholder='상품을 검색해 보세요!'
               value={inputValue}
               onInput={handleData}
               onKeyDown={onSubmitSearch}
             />
-            <IconButton type="button" sx={{ p: 1 }} onClick={searchProduct}>
-              <SearchIcon color="primary" />
+            <IconButton type='button' sx={{ p: 1 }} onClick={searchProduct}>
+              <SearchIcon color='primary' />
             </IconButton>
           </Paper>
           <Box
             sx={{
-              display: "flex",
-              gap: "26px",
-              ml: "auto",
-              alignItems: "center",
+              display: 'flex',
+              gap: '26px',
+              ml: 'auto',
+              alignItems: 'center',
             }}
           >
-            {userType === "BUYER" ? (
+            {userType === 'BUYER' ? (
               <>
-                <Link to="/cart">
+                <Link to='/cart'>
                   <Box sx={iconWrapper}>
-                    <img
-                      src={location.pathname === "/cart" ? cartGreen : cart}
-                      alt=""
-                    />
+                    <img src={location.pathname === '/cart' ? cartGreen : cart} alt='' />
                     장바구니
                   </Box>
                 </Link>
                 {!token ? (
-                  <Link to="/login">
+                  <Link to='/login'>
                     <Box sx={iconWrapper}>
-                      <img src={mypage} alt="" /> 로그인
+                      <img src={mypage} alt='' /> 로그인
                     </Box>
                   </Link>
                 ) : (
-                  <Link to="/mypage">
+                  <Link to='/mypage'>
                     <Box sx={iconWrapper}>
-                      <img src={mypage} alt="" /> 마이페이지
+                      <img src={mypage} alt='' /> 마이페이지
                     </Box>
                   </Link>
                 )}
               </>
             ) : (
               <>
-                <Link to="/mypage">
+                <Link to='/mypage'>
                   <Box sx={iconWrapper}>
-                    <img src={mypage} alt="" /> 마이페이지
+                    <img src={mypage} alt='' /> 마이페이지
                   </Box>
                 </Link>
                 <Button
                   onClick={handleSellerCenter}
-                  width="168px"
+                  width='168px'
                   style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    gap: "8px",
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    gap: '8px',
                   }}
                 >
-                  <img src={sellerCenter} alt="" />
+                  <img src={sellerCenter} alt='' />
                   판매자 센터
                 </Button>
               </>
@@ -185,10 +177,10 @@ const Header = ({ userType }) => {
         </Box>
       ) : (
         <Box sx={innerWrapper}>
-          <Link to="/">
-            <Box width="80px" component="img" src={logo} alt="호두마켓 로고" />
+          <Link to='/'>
+            <Box width='80px' component='img' src={logo} alt='호두마켓 로고' />
           </Link>
-          <Typography variant="h3" fontWeight="500" ml="16px">
+          <Typography variant='h3' fontWeight='500' ml='16px'>
             판매자 센터
           </Typography>
         </Box>
